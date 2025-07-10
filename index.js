cabd954ffe1889b01c8b40733d2b68e3bd540cd4
Iyaddiyaeldin <iyadaddiya@gmail.com>
2025-07-10 15:37:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,20 +16,26 @@ function createBot(botName) {
     setInterval(() => {
       bot.setControlState('jump', true);
       setTimeout(() => bot.setControlState('jump', false), 500);
-    }, 10000); // كل 10 ثواني يقفز
+    }, 10000);
 
     setInterval(() => {
       bot.chat('I am still here!');
-    }, 60000); // يرسل رسالة كل دقيقة
+    }, 60000);
+  });
+
+  bot.on('kicked', (reason) => {
+    console.warn(`${bot.username} was kicked:`, reason);
+    console.log(`Reconnecting ${botName} in 15 seconds...`);
+    setTimeout(() => createBot(botName), 15000);
   });
 
   bot.on('end', () => {
-    console.log(`${bot.username} got disconnected. Reconnecting...`);
-    setTimeout(() => createBot(botName), 5000);
+    console.log(`${bot.username} disconnected. Reconnecting in 10 seconds...`);
+    setTimeout(() => createBot(botName), 10000);
   });
 
   bot.on('error', (err) => {
-    console.error(`${botName} encountered an error:`, err);
+    console.error(`${botName} error:`, err);
   });
 }
 
@@ -37,7 +43,7 @@ function createBot(botName) {
 createBot('AFKBott1');
 createBot('AFKBott2');
 
-// خادم إكسبريس لإبقاء البوت نشطًا على Render
+// خادم Express لإبقاء البوت نشطًا على Render
 const app = express();
 app.get('/', (req, res) => res.send('Bots are running'));
 app.listen(3000, () => console.log('Keep-alive server is running on port 3000'));
